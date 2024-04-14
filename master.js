a721db3599f03fa130d74a36965fd30f5576ec1d
bigbk <34290288+bigbk@users.noreply.github.com>
2024-04-14 07:09:40 -0400

    Update master.js

--- master.js
+++ master.js
@@ -1,14 +1,21 @@
 function copyToClipboard(text) {
-    var dummy = document.createElement("textarea");
-    // to avoid breaking orgain page when copying more words
-    // cant copy when adding below this code
-    // dummy.style.display = 'none'
-    document.body.appendChild(dummy);
-    // Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-    dummy.value = text;
-    dummy.select();
-    document.execCommand("copy");
-    document.body.removeChild(dummy);
+//    var dummy = document.createElement("textarea");
+//    // to avoid breaking orgain page when copying more words
+//    // cant copy when adding below this code
+//    // dummy.style.display = 'none'
+//    document.body.appendChild(dummy);
+//    // Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+//    dummy.value = text;
+//    dummy.select();
+//    document.execCommand("copy");
+//    document.body.removeChild(dummy);
+
+    navigator.clipboard.writeText(text).then(function() {
+        console.log('Copying to clipboard was successful!');
+    }, function(err) {
+        console.error('Could not copy text: ', err);
+    });
+
 }
 
 
